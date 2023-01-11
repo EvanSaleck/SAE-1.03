dc10c54d770f6513565f6f0d11fbddb6c9aa7298
Evan Saleck <79723542+EvanSaleck@users.noreply.github.com>
2023-01-11 17:35:05 -0500

    Modif PHP
    Modifications finales du PHP afin de faire tout fonctionner pour tout les txt dans le dossier

--- sae103.php
+++ sae103.php
@@ -1,84 +1,63 @@
 <?php
 // Script qui permet de récupérer les données d'un fichier texte et de les stocker dans nos fichiers
 // Récupération du contenu de tout les fichiers txt du dossier utilisation de Joker qui reperent juste le .txt et qui execute sur tout les .txt
-$fileToTreat = __DIR__.'/fichiers.txt/*.txt';
+$fileToTreat = glob(__DIR__.'/fichiers.txt/*.txt');
 // Récupération du contenu du fichier
-$content = file_get_contents($fileToTreat);
-// Récuperation du nom du fichier pris en compte
-$fileName = basename($fileToTreat);
+foreach($fileToTreat as $file){
+    $content = file_get_contents($file);
+    // Récuperation du nom du fichier pris en compte
+    $fileName = pathinfo(basename($file), PATHINFO_FILENAME);
+    // Récupération du titre de la page 1 compris entre les balises DEBUT_TITRE et FIN_TITRE
+    $debutTitre = strpos($content,'DEBUT_TITRE') + strlen('DEBUT_TITRE');
+    $finTitre = strpos($content,'FIN_TITRE');
+    $titre = trim(substr($content,$debutTitre,$finTitre-$debutTitre));
+    // Récupération du texte de la page 1 compris entre les premières balises DEBUT_TEXTE et FIN_TEXTE
+    $premierDebutTexte = strpos($content,'DEBUT_TEXTE') + strlen('DEBUT_TEXTE');
+    $premierFinTexte = strpos($content,'FIN_TEXTE');
+    $descriptionTitre = trim(substr($content,$premierDebutTexte,$premierFinTexte-$premierDebutTexte));
+    // Récupération du titre du texte 2 compris entre les balises DEBUT_TITRE et FIN_TITRE
+    $debutTitre2 = strpos($content,'DEBUT_TITRE',$premierFinTexte) + strlen('DEBUT_TITRE');  // on commence à la fin du premier texte
+    $finTitre2 = strpos($content,'FIN_TITRE',$debutTitre2);
+    $titre2 = trim(substr($content,$debutTitre2,$finTitre2-$debutTitre2));
+    // Récupération du texte de la page 1 compris entre les balises DEBUT_TEXTE et FIN_TEXTE
+    $debutTexte = strpos($content,'DEBUT_TEXTE',$premierFinTexte) + strlen('DEBUT_TEXTE');  // on commence à la fin du premier texte 
+    $FinTexte = strpos($content,'FIN_TEXTE',$debutTexte);
+    $description = trim(substr($content,$debutTexte,$FinTexte-$debutTexte));
 
-// Récupération du titre de la page 1 compris entre les balises DEBUT_TITRE et FIN_TITRE
-$debutTitre = strpos($content,'SOUS_TITRE') + strlen('SOUS_TITRE');
-$finTitre = strpos($content,'FIN_TITRE');
-$titre = trim(substr($content,$debutTitre,$finTitre-$debutTitre));
+    //Ajoute dans notre fichier $fileName_texte.dat toute les données récupérées précédemment portant les noms de variables $titre, $descriptiontitre, $titre2, $description
 
-// Récupération du texte de la page 1 compris entre les premières balises DEBUT_TEXTE et FIN_TEXTE
-$premierDebutTexte = strpos($content,'DEBUT_TEXTE') + strlen('DEBUT_TEXTE');
-$premierFinTexte = strpos($content,'FIN_TEXTE');
-$descriptionTitre = trim(substr($content,$premierDebutTexte,$premierFinTexte-$premierDebutTexte));
+    file_put_contents(__DIR__.'/data_extraite/textes/'.$fileName.'_texte.dat',json_encode([
+        'titre' => $titre,
+        'descriptionTitre' => $descriptionTitre,
+        'titre2' => $titre2,
+        'description' => $description
+    ]));
 
-// Récupération du titre du texte 2 compris entre les balises DEBUT_TITRE et FIN_TITRE
-$debutTitre2 = strpos($content,'SOUS_TITRE',$premierFinTexte) + strlen('SOUS_TITRE');  // on commence à la fin du premier texte
-$finTitre2 = strpos($content,'FIN_TITRE',$debutTitre2);
-$titre2 = trim(substr($content,$debutTitre2,$finTitre2-$debutTitre2));
+   // Récupération des stats
+   $debutStats = strpos($content,'DEBUT_STATS') + strlen('DEBUT_STATS');
+   $finStats = strpos($content,'FIN_STATS');
+   $stats = trim(substr($content,$debutStats,$finStats-$debutStats));
 
-// Récupération du texte de la page 1 compris entre les balises DEBUT_TEXTE et FIN_TEXTE
-$debutTexte = strpos($content,'DEBUT_TEXTE',$premierFinTexte) + strlen('DEBUT_TEXTE');  // on commence à la fin du premier texte 
-$FinTexte = strpos($content,'FIN_TEXTE',$debutTexte);
-$description = trim(substr($content,$debutTexte,$FinTexte-$debutTexte));
 
-// Ajoute dans notre fichier $fileName_texte.dat toute les données récupérées précédemment portant les noms de variables $titre, $descriptiontitre, $titre2, $description 
-file_put_contents(__DIR__.'/data_extraite/'.$fileName.'_texte.dat',json_encode([
-    'titre' => $titre,
-    'descriptionTitre' => $descriptionTitre,
-    'titre2' => $titre2,
-    'description' => $description
-]));
-// Fichier Texte.dat encodé avec un aspect JSON assez facile à réutiliser avec des Webfiles
 
-// Récupération des stats
-$debutStats = strpos($content,'DEBUT_STATS') + strlen('DEBUT_STATS');
-$finStats = strpos($content,'FIN_STATS');
-$stats = trim(substr($content,$debutStats,$finStats-$debutStats));
+    // Ajoute dans notre fichier $fileName_stats.dat toute les données récupérées précédemment portant le nom de variable $stats avec une meilleure possibilité de travail des données donc séparée en plusieurs colonnes et titres
+    file_put_contents(__DIR__.'/data_extraite/tableaux/'.$fileName.'_tableau.dat',json_encode([
+        // Pour chaque antislash on crée une nouvelle colonne
+        'stats' => explode('\\',$stats)
+    ]));
 
-// si on veut retrailler les données
-$explodedStats = explode(PHP_EOL,$stats);
+    // Récupération des commerciaux
+    $debutMeilleursCommerciaux = strpos($content, 'MEILLEURS:') + strlen('MEILLEURS:');
+    $finMeilleursCommerciaux = strpos($content, PHP_EOL, $debutMeilleursCommerciaux);
 
-$statsForJson = [];
-if(count($explodedStats) > 0){
-    foreach($explodedStats as $lineStat){
-        $explodedLineStat = explode(',',$lineStat);
-        list($titre, $col1, $col2, $col3, $col4) = $explodedLineStat;
-        $statsForJson[] = [
-            'titre' => $titre,
-            'col1' => $col1,
-            'col2' => $col2,
-            'col3' => $col3,
-            'col4' => $col4
-        ];
-    }
+    $meilleurs = trim(substr($content,$debutMeilleursCommerciaux,$finMeilleursCommerciaux-$debutMeilleursCommerciaux));
+
+    // Ajoute dans notre fichier $fileName_commerciaux.dat toute les données récupérées précédemment portant le nom de variable $meilleurs avec une meilleure possibilité de travail des données donc séparée en plusieurs colonnes et titres
+    file_put_contents(__DIR__.'/data_extraite/comm/'.$fileName.'_comm.dat',json_encode([
+        // Pour chaque antislash on crée une nouvelle colonne
+        'meilleurs' => explode('\\',$meilleurs)
+    ]));
+
 }
 
-file_put_contents(__DIR__.'/data_extraite/'.$fileName.'tableau.dat',json_encode($statsForJson));
-
-// Récupération des commerciaux
-$debutMeilleursCommerciaux = strpos($content, 'MEILLEURS:') + strlen('MEILLEURS:');
-$finMeilleursCommerciaux = strpos($content, PHP_EOL, $debutMeilleursCommerciaux);
-
-$meilleurs = trim(substr($content,$debutMeilleursCommerciaux,$finMeilleursCommerciaux-$debutMeilleursCommerciaux));
-$meilleursForJson = [];
-
-$explodedMeilleurs = explode(',',$meilleurs);
-if(count($explodedMeilleurs) > 0){
-    foreach($explodedMeilleurs as $lineMeilleur){
-        $regex = '/(.*)\/(.*)=(.*)/';
-        preg_match($regex,$lineMeilleur,$output);
-        $meilleursForJson[] = [
-            'code' => $output[1],
-            'nom' => $output[2],
-            'ca' => $output[3]
-        ];
-    }
-}
-
-file_put_contents(__DIR__.'/data_extraite/'.$fileName.'comm.dat',json_encode($meilleursForJson));
+?>
