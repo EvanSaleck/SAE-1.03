8ad0ff706783f323bf6ea6cbe5573761701790e9
Evan Saleck <79723542+EvanSaleck@users.noreply.github.com>
2023-01-18 16:20:58 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,14 +1,18 @@
- #!/bin/bash
+#!/bin/bash
 
- #Convertion des fichiers svg en png
- for values in *.svg #On parcours toutes les images svg
- do 
-     convert "$values" "${values:0:3}.png" #on convertit les images en png
- done
+fichier="photosfinal"
 
+if [ $# -eq 1 ] then
+    if [ ! -d photos/output ]; then
+        mkdir -p photos/output;
+    fi
 
-for values in *.png #On parcours toutes les images png
-do 
-        docker run --rm -v $(pwd)/tetes:/work bigpapoo/sae103-imagick "magick $nomSvg -resize 200x200 -gravity North -extent 200x185 -resize 200x200 -colorspace Gray ./output/$nomFichier.png"
-done
-
+    for photo in photos/*.svg; do
+        nomSvg=$(echo $photo | cut -d '/' -f2)
+        nomFichier=$(echo $nomSvg | cut -d '.' -f1)
+        echo ./photos/output/$nomFichier.png
+        docker run --rm -v $(pwd)/photos:/work bigpapoo/sae103-imagick "magick $nomSvg -resize 200x200^ -gravity North -extent 200x185 -colorspace Gray ./output/$nomFichier.png"
+    done
+else
+    echo "Utilisation: ./avatars.sh [dossier-images]"
+fi
