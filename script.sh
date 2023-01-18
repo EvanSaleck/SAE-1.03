494c0bb30ce9f6c74c49d14a625969157d08923e
Evan Saleck <79723542+EvanSaleck@users.noreply.github.com>
2023-01-18 16:27:03 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 
-fichier="photosfinal"
-
-if [ $# -eq 1 ] then
-    if [ ! -d photos/output ]; then
-        mkdir -p photos/output;
+if [ $# -eq 1 ]; then
+    if [ ! -d $1/output ]; then
+        mkdir -p $1/output;
     fi
 
-    for photo in photos/*.svg; do
+    for photo in $1/*.svg; do
         nomSvg=$(echo $photo | cut -d '/' -f2)
         nomFichier=$(echo $nomSvg | cut -d '.' -f1)
-        echo ./photos/output/$nomFichier.png
-        docker run --rm -v $(pwd)/photos:/work bigpapoo/sae103-imagick "magick $nomSvg -resize 200x200^ -gravity North -extent 200x185 -colorspace Gray ./output/$nomFichier.png"
+        echo ./$1/output/$nomFichier.png
+        docker run --rm -v $(pwd)/$1:/work bigpapoo/sae103-imagick "magick $nomSvg -resize 200x200^ -gravity North -extent 200x185 -colorspace Gray ./output/$nomFichier.png"
     done
 else
     echo "Utilisation: ./avatars.sh [dossier-images]"
